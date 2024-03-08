5768ec8cecb7045ba75240fed001ed0c994355b6
Anurag Singh <anuragsngh2615@gmail.com>
2024-03-09 01:46:16 +0530

    first commit

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import { Appbar } from "../components/Appbar"
+
+import { Users } from "../components/Users"
+
+export const Dashboard = () => {
+    return <div>
+        <Appbar />
+        <div className="m-8">
+            
+            <Users />
+        </div>
+    </div>
+}
